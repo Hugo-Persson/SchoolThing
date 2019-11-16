7848301fa3700d88afe91ad19014c3f4cd33703d
Hugo Persson <hugopersson7@outlook.com>
2019-11-16 19:17:18 +0100

    added basic login page

--- Client/src/Pages/Login.svelte
+++ Client/src/Pages/Login.svelte
@@ -1,7 +1,39 @@
 <script>
+  import { slide } from "svelte/transition";
+
+  let email;
+  let register = false;
+  console.log("start");
+  let headerText = "Login/Register";
   function getOneTimeCode(e) {
     e.preventDefault();
+    console.log(register);
+
+    if (register) {
+      registerUser();
+      console.log("wrong");
+      return;
+    }
+    register = !register;
+    headerText = "Register";
     console.log("hey");
+    if (!email) {
+      return;
+    }
+
+    const init = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: {}
+    };
+    getUniqueNumberFetch = fetch();
+  }
+  let getUniqueNumberFetch;
+  function login() {}
+  function registerUser() {
+    console.log("yia");
   }
 </script>
 
@@ -12,9 +44,24 @@
 </style>
 
 <main>
-  <h1>Login/Register</h1>
-  <form action="" on:click={login}>
-    <input type="text" name="email" id="enterEmail" placeholder="E-Mail" />
+  <header>
+    <h1>{headerText}</h1>
+  </header>
+  <form action="" on:submit={register ? registerUser : getOneTimeCode}>
+    {#if register}
+      <br />
+      <div transition:slide>
+        <input type="text" name="Name" id="" placeholder="Your name" />
+      </div>
+    {/if}
+    <input
+      type="text"
+      name="email"
+      bind:value={email}
+      id="enterEmail"
+      placeholder="E-Mail" />
+
+    <br />
     <input type="submit" value="Proceed" />
 
   </form>
